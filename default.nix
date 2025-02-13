93f51004b0580e712c0fd33a7e4ca3c0e25bd4dc
Coca <coca16622@gmail.com>
2025-02-13 18:59:19 +0000

    Add basic `default.nix`

---
+++ default.nix
@@ -0,0 +1,11 @@
+{pkgs ? import <nixpkgs> {}}: let
+  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
+  pkgs.rustPlatform.buildRustPackage {
+    pname = manifest.name;
+    version = manifest.version;
+    cargoLock.lockFile = ./Cargo.lock;
+    src = pkgs.lib.cleanSource ./.;
+    meta.mainProgram = manifest.name;
+    meta.description = manifest.description or null;
+  }
