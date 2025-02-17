9320ef515ff1e8598d8637fd4b4f5d9c4a18e1c5
Coca <coca16622@gmail.com>
2025-02-17 12:16:18 +0000

    Switch to crane for nix building

--- default.nix
+++ default.nix
@@ -1,11 +1,18 @@
 {pkgs ? import <nixpkgs> {}}: let
-  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+  crane = pkgs.fetchzip {
+    url = "https://github.com/ipetkov/crane/archive/bef2b45cd1273a9e621fb5292de89f4ed59ad812.tar.gz";
+    sha256 = "1w5lw78j5x5p7al6kxyk48rd00821fws44s1050armzqnkdqiy54";
+  };
+  craneLib = import crane {inherit pkgs;};
+
+  commonArgs = {
+    src = craneLib.cleanCargoSource ./.;
+    strictDeps = true;
+  };
 in
-  pkgs.rustPlatform.buildRustPackage {
-    pname = manifest.name;
-    version = manifest.version;
-    cargoLock.lockFile = ./Cargo.lock;
-    src = pkgs.lib.cleanSource ./.;
-    meta.mainProgram = manifest.name;
-    meta.description = manifest.description or null;
+  craneLib.buildPackage commonArgs
+  // {
+    # Allow for reuse of previous dependency builds
+    cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+    meta.mainProgram = "diski";
   }
